870dab62be2724a50ffa112be9d846d6ee43acd9
lindiwe Isiwele <isiwelelee@gmail.com>
2023-11-16 22:24:27 +0000

    sorting

--- CSVsortingKeta/Program.cs
+++ CSVsortingKeta/Program.cs
@@ -12,89 +12,102 @@ namespace CSVsortingKeta
     class Program
     {
 
-      public  static void Main ()
+
+        static void Main()
         {
             //link the text file to read as a string
             {
-                string filePath = @"D:\C#learning\sorting.txt";
+                string filePath = @"D:\c#learning\sorting.txt";
 
-                //string[] lines = File.ReadAllLines(filePath);
 
                 // Convert the text file into a list
-                List<string> lines = new List<string>();
+
                 List<Customer> customers = new List<Customer>();
 
-                lines = File.ReadAllLines(filePath).ToList();
+
                 /*user input to select and filter fields option 1. Firstname 2.Lastname 3.Date of Birthday
                 once the user chooses an option only the select data should be printed to console */
+
+                Console.WriteLine("Select sorting option (1: First Name, 2: LastName, 3: Date of Birth):");
+
+
+
+                //read the string, datetime format yyyy-dd-MM
+
+
                 {
-                    Console.WriteLine("Select sorting option (1: First Name, 2: LastName, 3: Date of Birth):");
 
-                   
-                    
-                    //read the string, datetime format yyyy-dd-MM
-                    if (int.TryParse(Console.ReadLine(), out int sortingOption) && sortingOption >= 0 && sortingOption <= 2)
+                    string[] items = File.ReadAllLines(filePath);
+
+
+                    foreach (string item in items)
                     {
-                        //                  List<Customer> customers = ReadCustomersFromCsv(filePath);
+                        items = item.Split(','); // split the data at the comma 
 
-                        if (sortingOption == 1)
-                            customers = customers.OrderBy(c => c.FirstName).ThenBy(c => c.LastName).ThenBy(c => c.DateOfBirth).ToList();
-                        else if (sortingOption == 2)
-                            customers = customers.OrderBy(c => c.LastName).ThenBy(c => c.FirstName).ThenBy(c => c.DateOfBirth).ToList();
-                        else if (sortingOption == 3)
-                            customers = customers.OrderBy(c => c.DateOfBirth).ToList();
-
-                        customers = customers.Where(c => c.DateOfBirth != DateTime.MinValue).ToList();
-
-                        Console.WriteLine("Sorted and filtered customer data:");
-                        foreach (var customer in customers) // used var to enable reading different data types
-                        {
-                            Console.WriteLine($"{customer.FirstName} {customer.LastName} - {customer.DateOfBirth:yyyy-MM-dd}");
-                        }
-                    }
-                    else
-                    {
-                        Console.WriteLine("Invalid sorting option. Please enter 1, 2, or 3. You entered: " + sortingOption.ToString());
-
-                    }
-
-
-                    foreach (string line in lines)
-                    {
-                        string[] items = line.Split(','); // split the data at the comma 
-
-                        if (line.Length >= 3 && DateTime.TryParseExact(lines[2], "yyyy-MM-dd", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime dateOfBirth))
+                        if (items.Length >= 3 && DateTime.TryParseExact(items[2], "yyyy-MM-dd", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime dateOfBirth))
                         {
 
 
                             customers.Add(new Customer
                             {
-                                FirstName = lines[0],
-                                LastName = lines[1],
+                                FirstName = items[0],
+                                LastName = items[1],
                                 DateOfBirth = dateOfBirth,
                             });
-                            {
-                                {
-
-
-                                    Console.ReadLine();
-
-                                }
-                            }
                         }
+
                     }
+
+                    //Apply sorting according to user choice
+                    int sortingOption = 0;
+                    if (sortingOption == 1)
+                        customers = customers.OrderBy(c => c.FirstName).ThenBy(c => c.LastName).ThenBy(c => c.DateOfBirth).ToList();
+                    else if (sortingOption == 2)
+                        customers = customers.OrderBy(c => c.LastName).ThenBy(c => c.FirstName).ThenBy(c => c.DateOfBirth).ToList();
+                    else if (sortingOption == 3)
+                        customers = customers.OrderBy(c => c.DateOfBirth).ToList();
+
+                    customers = customers.Where(c => c.DateOfBirth != DateTime.MinValue).ToList();
+
+                    Console.WriteLine("Sorted and filtered customer data:");
+
+                    for (int i = 0; i < customers.Count; i++) // used var to enable reading different data types
+                    {
+                        Customer customer = customers[i];
+                        Console.WriteLine($"{customer.FirstName} {customer.LastName} - {customer.DateOfBirth:yyyy-MM-dd}");
+
+
+
+                        // had to comment out the else because can'nt start  else with a statement
+
+              //  else
+                        
+                           // Console.WriteLine("Invalid sorting option. Please enter 1, 2, or 3. You entered: " + sortingOption.ToString());
+                        
+
+
+
+                        //List<Customer> customers = ReadCustomersFromCsv(filePath);
+
+                        //lines = File.ReadAllLines(filePath).ToList();
+                        Console.ReadLine();
+
+                    }
+
+
                 }
+
             }
+
         }
     }
-
 }
 
 
 
 
-                        
 
-      
-        
-    
+
+
+
+
